f378e2967b7f24315688f3ddd1a968e76394d81b
Tiago Branco <tiagobrancocampos08@gmail.com>
2025-10-03 15:50:47 +0100

    Este e o meu primeiro Commit

---
+++ m17c-ex01/m17c-ex01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace m17c_ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eu sou o Tiago");
+        }
+    }
+}
